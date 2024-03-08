898ee88b483a0be0feeff302b73e9010578c0025
tao.chen <tao.chen@xenbots.com>
2024-03-08 11:32:01 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,11 @@ mod utils;
 
 pub use error::{Error, ErrorKind};
 pub use template::Template;
+
+
+#[test]
+pub fn test_simple_3() {
+    let template = "AA{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}BB";
+    let text =formatx!(template, 24,3,6,16,40,23);
+    println!("{}", text.unwrap());
+}
